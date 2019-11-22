e27d859c5cdb3b04870ee64c2cb3bb398ea44474
jucastano <me@jucastano.com>
2019-11-22 01:22:29 -0500

    Changing GreatMoods
    Changed GreatMoods to FundraisingATM for Sales Coordinator

--- sales/memberAZ.php
+++ sales/memberAZ.php
@@ -6,7 +6,7 @@
             exit;
        }
      $userID = $_SESSION['userId'];
-     
+
      $query = "SELECT * FROM user_info 	WHERE userInfoID='$userID'";
      $result = mysqli_query($link, $query)or die ("couldn't execute query.".mysql_error($link));
      $row = mysqli_fetch_assoc($result);
@@ -17,45 +17,59 @@
 <head>
 	<title>Title |Members A to Z</title>
 </head>
-	
+
 <body>
 <div id="container">
       <?php include 'header.inc.php' ; ?>
       <?php include 'sidenav.php' ; ?>
-      
+
     <div id="content">
     <h1>Students or Members - A to Z </h1>
-    
+
     <h3>Achieving Success for your Goal is our Goal... So Let’s Do It!!!</h3>
 
       		<h5>Student/ Member Step by Step Fundraising Checklist </h5>
-      		<ul>
+      		<ol>
       		  <li> When working with students, tell them that Volunteering to be on Fundraising Leaders Team will be a Great College Application Reference </li>
       		  <li>Help the Fundraising Leaders Setup their Main Website  </li>
       		  <li>Setup your Personalized Fundraising Website </li>
       		  <li>Setup your Friends and Family Supporters List and add local Businesses </li>
       		  <li>Add a link to your Twitter and Facebook Page connecting it to your Fundraising Website </li>
-      		  <li>Start the Email Campaign Kickoff  </li>   
-      	        </ul>
-      	        
+      		  <li>Start the Email Campaign Kickoff  </li>
+          </ol>
+          <br>
+
       		<h5>Year Round Opportunity to Generate Funds </h5>
+          <ul class="bulletedlist">"
       		  <li>Holidays </li>
-      		  <li>Apparel Seasons</li>		 
-      		  
-      		<h5>The GreatMoods Mall Products and Gifts </h5>
+      		  <li>Apparel Seasons</li>
+          </ul>
+          <br>
+
+      		<h5>The FundraisingATM Mall Products and Gifts </h5>
+          <ul class="bulletedlist">
+      		  <li>Products for every reason and season</li>
+      		  <li>Something for everyone</li>
+      		</ul>
+          <br>
+
       		<h5>The Student Member Potential Prospects </h5>
+          <ul class="bulletedlist">"
       		  <li>Friends & Family   </li>
       		  <li>Local Businesses   </li>
                   <li>Meeting and Events   </li>
-      		
+          </ul>
+          <br>
+
                 <h5>Fundraising Tools </h5>
+                <ul class="bulletedlist">
                   <li>Video Training   </li>
       		  <li>Tablet or Laptop Presentation   </li>
                   <li>Forms at Events   </li>
                   <li>Contact Cards   </li>
-
+                </ul>
       </div>  <!--end content-->
-      
+
 	<?php include 'footer.php' ; ?>
 </div> <!--end container-->
 
