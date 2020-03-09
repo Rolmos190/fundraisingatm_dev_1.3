b97cdec03874c2d553fa7feca12844574567b97a
Roshan Jamkatel <roshan.jamkatel@gmail.com>
2020-03-09 16:58:25 +0100

    addSupplier.php modification
    Fixed php issues on addSupplier.php
    Documentation in Word Document

--- executive/supplierNew.php
+++ executive/supplierNew.php
@@ -7,11 +7,11 @@
    $id = $_SESSION['userId'];
    //$link = connectTo();
    $link = connectTo2();
-   
+
    $servername = "localhost";
-   $username = "amoodf5_dssupply";
+   $username = "gogrea6_amoodf5_dssupply";
    $psw = "_s0]oX0.yh36";
-   $dbname = "amoodf5_supply";
+   $dbname = "gogrea6_amoodf5_supply";
    $conn = new mysqli($servername, $username, $psw, $dbname);
 
     // Check connection
@@ -20,7 +20,7 @@
     }
      if(isset($_POST['submit']))
     {
-     
+
      $supName = mysqli_real_escape_string($link, $_POST['sn']);
      $supPhone = mysqli_real_escape_string($link, $_POST['phone']);
      $supAddress = mysqli_real_escape_string($link, $_POST['ad']);
@@ -30,29 +30,30 @@
      $supEmail = mysqli_real_escape_string($link, $_POST['email']);
      $supPass = mysqli_real_escape_string($link, $_POST['pass']);
      $supCode = mysqli_real_escape_string($link, $_POST['code']);
-     
      $password = hash('sha256', $supPass);
-     
+     $url = "index.html";
+     $urlTwo = "index.html"; // Wholesale site? Which link should be used?
+
       // prepare and bind
-     $stmt = $conn->prepare("INSERT INTO users (userName, userEmail, supplyCode, phone, address, city, state, zip, rawPass, userPass) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-     $stmt->bind_param("ssssssssss", $supName, $supEmail, $supCode, $supPhone, $supAddress, $supCity, $supState, $supZip, $supPass, $password);
+     $stmt = $conn->prepare("INSERT INTO users (userName, userEmail, supplyCode, phone, address, city, state, zip, rawPass, userPass, url1, url2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+     $stmt->bind_param("ssssssssssss", $supName, $supEmail, $supCode, $supPhone, $supAddress, $supCity, $supState, $supZip, $supPass, $password, $url, $urlTwo);
 
      //$stmt->execute();
-     
-     if ($stmt->execute()) 
-     { 
+
+     if ($stmt->execute())
+     {
           // it worked
           echo "New records created successfully";
           header("Location: viewSuppliers.php");
 
-     } 
+     }
      else
      {
          // it didn't
-         echo $stmt->error; 
+         echo $stmt->error;
      }
 
-     
+
      $stmt->close();
      $link->close();
      /*
@@ -70,5 +71,5 @@
      }
      */
    }
-   
+
 ?>
