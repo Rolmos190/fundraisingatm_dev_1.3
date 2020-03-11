92d9c91f55f034eb306bcbdd411560cadd92edff
Roshan Jamkatel <roshan.jamkatel@gmail.com>
2020-03-11 16:09:13 +0100

    Update editSupplier.php
    Fixed database issue (wrong database names and usernames supplied)

--- executive/editSupplier.php
+++ executive/editSupplier.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
    $servername = "localhost";
-   $username = "amoodf5_dssupply";
+   $username = "gogrea6_amoodf5_dssupply";
    $password = "_s0]oX0.yh36";
-   $dbname = "amoodf5_supply";
+   $dbname = "gogrea6_amoodf5_supply";
    ob_start();
    // Create connection
    $conn = new mysqli($servername, $username, $password, $dbname);
    // Check connection
-  
-   
+
+
    if (!$conn)
    {
      die("Connection failed: " . mysqli_connect_error());
    }
    if(isset($_POST['submit']))
-   { 
+   {
       $sn = $_POST['sn'];
       $phn = $_POST['phone'];
       $adds = $_POST['ad'];
@@ -26,11 +26,11 @@
       $psw = $_POST['pass'];
       $psswd = hash('sha256', $psw);
       $record = $_POST['record'];
-      
-      $stmt = $conn->prepare("UPDATE users SET userName = ?, 
-      userEmail = ?, 
-      supplyCode = ?,  
-      phone = ?,  
+
+      $stmt = $conn->prepare("UPDATE users SET userName = ?,
+      userEmail = ?,
+      supplyCode = ?,
+      phone = ?,
       address = ?,
       city = ?,
       state = ?,
@@ -38,12 +38,12 @@
       rawPass = ?,
       userPass = ?
       WHERE userId = ?");
-      
+
       $stmt->bind_param('ssssssssssi',
       $sn,
       $em,
       $cde,
-      $phn, 
+      $phn,
       $adds,
       $cty,
       $st,
@@ -52,17 +52,17 @@
       $psswd,
       $record
       );
-  
-     if ($stmt->execute()) 
+
+     if ($stmt->execute())
      {
         //echo "Record updated successfully";
         header("Location: viewSuppliers.php");
-     } 
-     else 
+     }
+     else
      {
        echo "Error updating record: " . mysqli_error($conn);
      }
       $stmt->close();
    }
-   
+
 ?>
